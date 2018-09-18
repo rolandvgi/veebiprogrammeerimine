5289e47dce71db8d145124373d7520382d5607a1
Roland Vägi <rolandvgi@users.noreply.github.com>
2018-09-18 11:33:39 +0300

    lisatud kataloogi sisu lugemine, massiivid, tsükkel

--- tund3/index.php
+++ tund3/index.php
@@ -3,6 +3,11 @@
 	$firstName = "Roland";
 	$lastName = "Vägi";	
 	$dateToday = date("d.m.Y");
+	$weekdayNow = date("N");
+	$weekdayNamesET = ["esmaspäev", "teisipäev","kolmapäev","neljapäev","reede","laupäev","pühapäev"];
+	//echo $weekdayNamesET[1];
+	//var_dump($weekdayNamesET);
+	//echo $weekdayNow;
 	$hourNow = date("G");
 	$partOfDay = "";
 	if ($hourNow < 8){
@@ -14,7 +19,11 @@
 	if ($hourNow >= 16){
 		$partOfDay = "ilmselt vaba aeg";
 	}
-		
+	$picNum = mt_rand(2, 43);
+	//echo $picNum;		
+	$picUrl = "http://www.cs.tlu.ee/~rinde/media/fotod/TLU_600x400/tlu_";
+	$picEXT = ".jpg";
+	$picFile = $picUrl .$picNum .$picEXT;
 ?>
 
 <!DOCTYPE html>
@@ -37,18 +46,23 @@
 	?>
 	</h1>					<!-- target blank on uus vahekaart!-->
 	<p>See leht on loodud <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames, ei pruugi parim välja näha ning kindlasti ei sisalda tõsiseltvõetavat sisu!</p>
+	
+	<p>Tundides tehtu: <a href="photo.php">photo.php</a></p>
 	<?php												// \n= uus rida
-	echo "<p>Tänane kuupäev on: " .$dateToday .".</p> \n";
+	//echo "<p>Tänane kuupäev on: " .$dateToday .".</p> \n";
+	//echo  "<p>Täna on ".$weekdayNow .", " .$dateToday .".</p> \n";
+	echo  "<p>Täna on ".$weekdayNamesET[$weekdayNow - 1] .", " .$dateToday .".</p> \n";
 	echo "<p>Lehe avamise hetkel oli kell " .date("H:i:s ") ."käes oli ". $partOfDay . ".</p> \n";
 	?>
 	<p>Koduseks tööks oli vaja lisada uus rida, mida ma ka tegin.</p>
 	<!--<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_3.jpg" 
 	alt="TLÜ Terra õppehoone">!-->
 	
-	<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_3.jpg" 
-	alt="TLÜ Terra õppehoone">
-	<p>Mul on ka sõber, kes teeb oma <a href="../~urmoros/">veebi</a></p>
-	<button a href="http://neti.ee">Vajuta siia, kuigi nupp ei tee midagi</button>
+	<!--<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_3.jpg" 
+	alt="TLÜ Terra õppehoone">-->
+	<img src ="<?php echo $picFile; ?>" alt="TLÜ Terra õppehoone">
+	<p>Mul on ka sõber, kes teeb oma <a href="../../../~urmoros/">veebi</a></p>
+	<button formaction="http://www.neti.ee">"Vajuta siia, kuigi nupp ei tee midagi</button>
 	
 	
 
